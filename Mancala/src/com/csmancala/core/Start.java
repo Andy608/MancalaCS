af4199619904b72627da3ecaf7efd7c45745d5c2
Andy608 <AndrewR608@gmail.com>
2015-03-20 12:20:18 -0400

    Added basic start files.

---
+++ Mancala/src/com/csmancala/core/Start.java
@@ -0,0 +1,19 @@
+package com.csmancala.core;
+
+import javax.swing.SwingUtilities;
+
+public class Start {
+	
+	public static Mancala gameInstance;
+	
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+			}
+		});
+	}
+}
