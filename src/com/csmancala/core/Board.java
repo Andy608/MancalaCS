ddbd8aa5508fa9cea53a4079a9db4a1737a3a152
Andy608 <AndrewR608@gmail.com>
2015-03-20 22:39:54 -0400

    Added getters for player objects in Board class and fixed 2D array to be initialized in x y order instead of y x order.

--- src/com/csmancala/core/Board.java
+++ src/com/csmancala/core/Board.java
@@ -5,31 +5,33 @@ public class Board {
 	//The total number of stones on the board.
 	public static final int STONE_AMOUNT = 50;
 	
-	private Pit boardArray [][];
-	private Player player1, player2;
+	private static final int PIT_WIDTH = 6;
+	private static final int PIT_HEIGHT = 2;
+	private Pit boardArray[][];
+	private Player player1;
+	private Player player2;
 	
-	public Board(Pit myBoardArray[][]){
-		
-		this.boardArray = new Pit[2][6];
+	public Board(Pit myBoardArray[][]) {
+		this.boardArray = new Pit[PIT_WIDTH][PIT_HEIGHT];
 	}
 	
-	public void makeMove(int r, int c){
+	public void makeMove(int r, int c) {
 		// alot to do here
 	}
 	
-	private boolean willLandInGoal(){
+	private boolean willLandInGoal() {
 		return true; // Just to keep it error free i added a return statement
 	}
 	
-	private boolean willLandInEmptyPit(int r, int c){
+	private boolean willLandInEmptyPit(int r, int c) {
 		return true;
 	}
 	
-	private void pointsGained(Player p, int pointValue){
+	private void pointsGained(Player p, int pointValue) {
 		//add points to player
 	}
 	
-	public int winStatus(){
+	public int winStatus() {
 		/**
 		 * return -1 = tie
 		 * return 0 = game not complete
@@ -39,19 +41,17 @@ public class Board {
 		return 0;
 	}
 	
-	@Override
-	public String toString(){
-		String s = "";
-		
-		for(int i = 0; i < 2; i++){
-			for(int j = 0; j < 6; j++){
-				s += this.boardArray[i][j];
-			}
-			
-			s += "\n"; // not sure if this works as intended
-			
-		}
-		return s;
+	/**
+	 * @return the player1 object.
+	 */
+	public Player getPlayer1() {
+		return player1;
 	}
 	
+	/**
+	 * @return the player2 object.
+	 */
+	public Player getPlayer2() {
+		return player2;
+	}
 }
