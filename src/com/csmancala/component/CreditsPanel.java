abcdffaadd172cec7b254c3a83dafe3828920da6
“picklyjoe” <matgar1997@gmail.com>
2015-03-23 18:27:20 -0400

    removed not needed spaces/added 2 comp files/fixed name loop

---
+++ src/com/csmancala/component/CreditsPanel.java
@@ -0,0 +1,17 @@
+package com.csmancala.component;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class CreditsPanel extends JPanel implements ActionListener{
+
+	private static final long serialVersionUID = 6954435285087527912L;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
