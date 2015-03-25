5e79175e27d92909a7fa362e8387de29255f14e1
“picklyjoe” <matgar1997@gmail.com>
2015-03-24 22:38:36 -0400

    changed rules panel from png to jlabel

--- src/com/csmancala/component/RulesPanel.java
+++ src/com/csmancala/component/RulesPanel.java
@@ -4,6 +4,7 @@ import java.awt.Component;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,10 +23,13 @@ public class RulesPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 695443524538627912L;
 	
-	private JPanel instructionsPanel;
-	private JLabel mancalaLogo = new JLabel("Mancala FIX ME!!!");
-	private JLabel instructionsLabel = new JLabel(new ImageIcon(ResourceLoader.MANCALA_INSTRUCTIONS));
+	private JPanel instructionsPanel = new JPanel();
+	private JLabel mancalaLogo = new JLabel("Mancala Remove White space!!!");
+	//private JLabel instructionsLabel = new JLabel(new ImageIcon(ResourceLoader.MANCALA_INSTRUCTIONS));
 	private JButton returnButton = new JButton("Return to Main Menu");
+	private JLabel instructionsLabelObject = new JLabel();
+	private JLabel instructionsLabelHowPlay = new JLabel();
+	private JLabel instructionsLabelHowWin = new JLabel();
 	
 	public RulesPanel() {
 		super();
@@ -43,13 +47,22 @@ public class RulesPanel extends JPanel implements ActionListener {
 	
 	public void setupInstructions() {
 		
+		initLabelWithText();
 		//PLEASE MAKE THE NEW RULES IMAGE OVERLAY THE BACKGROUND. PLEASE ADD THE BUTTON AND TITLE BACK ON OVER THE PANEL ASWELL!!
-		this.instructionsPanel = new JPanel();
-		this.instructionsPanel.add(instructionsLabel);
-		
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		//this.instructionsPanel.add(instructionsLabel);
+		instructionsPanel.setLayout(new BoxLayout(instructionsPanel, BoxLayout.PAGE_AXIS));
 		mancalaLogo.setFont(new Font("Montserrat", Font.BOLD, 72));
 		mancalaLogo.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		instructionsPanel.add(instructionsLabelObject);
+		instructionsPanel.add(instructionsLabelHowPlay);
+		instructionsPanel.add(instructionsLabelHowWin);
+		
+		//this.instructionsLabelHowPlay.setBackground(null); THIS DOESNT WORK
+		
+		instructionsLabelObject.setFont(new Font("Montserrat", Font.PLAIN, 20));
+		instructionsLabelHowPlay.setFont(new Font("Montserrat", Font.PLAIN, 20));
+		instructionsLabelHowWin.setFont(new Font("Montserrat", Font.PLAIN, 20));
 		this.add(mancalaLogo);
 ////		instructionsLabel.setHorizontalAlignment(JLabel.CENTER);
 //		this.add(instructionsLabel);
@@ -64,5 +77,21 @@ public class RulesPanel extends JPanel implements ActionListener {
 			Start.getMancala().returnToMenu();
 		}
 	}
+	
+	public void initLabelWithText() {
+		String instructionsLabelObjectText = "<html> OBJECT <br/>Have the most stones in your mancala after all the"
+				+ "all the stones on your game Board are captured<br/><br/>";
+		String instructionsLabelHowPlayText = "<html>HOW TO PLAY<br/>on your turn choose a slot to pick up all your stones"
+				+ "and going counter clock-wise stones will fall into<br/> the following slots including your goal."
+				+ "After you do this and capture any stones your turn is over.<br/>"
+				+ "Unless your final stone in your hand falls in your goal, in which case you go again<br/>"
+				+ "You also gain stones if your final stone land in an empty slot, then you take all<br/> stones from the oppose side and placed them in your goal!<br/><br/>";
+		String instructionLabelHowWinText = "<html>HOW TO WIN<br/>The player with the most stones in their goal after no stones are left on the board wins!<br/>"
+				+ "HAVE FUN";
+		instructionsLabelObject.setText("<html><div style=\"text-align: center;\">" + instructionsLabelObjectText + "</html>");
+		instructionsLabelHowPlay.setText("<html><div style=\"text-align: center;\">" + instructionsLabelHowPlayText + "</html>");
+		instructionsLabelHowWin.setText("<html><div style=\"text-align: center;\">" + instructionLabelHowWinText + "</html>");
+	}
+	
 
 }
